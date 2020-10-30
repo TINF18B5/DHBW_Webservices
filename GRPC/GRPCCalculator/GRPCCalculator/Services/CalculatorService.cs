7bb847235c3ba1e4144f690e9381620fd12a0e59
Lukas Panni <lukas.panni@outlook.de>
2020-10-30 11:12:51 +0100

    GRPC Calc

---
+++ GRPC/GRPCCalculator/GRPCCalculator/Services/CalculatorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GRPCCalculator.Protos;
+
+namespace GRPCCalculator.Services
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        public override Task<Result> Add(Argument request, ServerCallContext context)
+        {
+            return Task.FromResult(new Result()
+            {
+                Result_ = request.Arg1 + request.Arg2
+            });
+        }
+    }
+}
---
+++ GRPC/GRPCCalculator/GRPCClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Grpc.Net.Client;
+using GRPCCalculator.Protos;
+
+namespace GRPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001"))
+            {
+                var invoker = channel.CreateCallInvoker();
+                Calculator.CalculatorClient client = new Calculator.CalculatorClient(invoker);
+                var argument = new Argument() {Arg1 = 36, Arg2 = 6};
+                var result = client.Add(argument);
+                Console.WriteLine($"{argument.Arg1} + {argument.Arg2} = {result.Result_}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
