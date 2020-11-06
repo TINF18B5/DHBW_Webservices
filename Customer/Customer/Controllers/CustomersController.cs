bf345c205dbf85dbfe9496c2c3b4fcb2d79201e5
Lukas Panni <lukas.panni@outlook.de>
2020-11-06 12:18:21 +0100

    REST

---
+++ Customer/Customer/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Model.Customer> Get()
+        {
+            var customers = new List<Model.Customer>();
+            customers.Add(new Model.Customer() { Id = 1, Name = "Lukas Panni", Street = "LOL", City = "Odenheim", ZipCode = "76684" });
+            customers.Add(new Model.Customer() { Id = 2, Name = "Test Person", Street = "Straße", City = "Bruchsal", ZipCode = "76646" });
+            return customers;
+
+        }
+    }
+}
---
+++ Customer/Customer/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace Customer.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
